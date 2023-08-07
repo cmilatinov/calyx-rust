d2861f52bf44c9ed93e99b4dd3d0f022d61bfc65
Ruben <ruben.e.sanchez.a@gmail.com>
2023-08-07 19:28:07 -0400

    Added some basic variables for Mesh

--- engine/src/assets/mesh.rs
+++ engine/src/assets/mesh.rs
@@ -1,1 +1,28 @@
-pub struct Mesh;
+use glm::{Mat4, Vec2, Vec3};
+
+pub struct Mesh {
+    indices: Vec<u32>,
+    vertices: Vec<Vec3>,
+    normals: Vec<Vec3>,
+    uvs: (Vec<Vec2>,
+         Vec<Vec2>,
+         Vec<Vec2>,
+         Vec<Vec2>),
+    instances: Vec<Mat4>
+}
+
+impl Mesh {
+    pub fn new() -> Self {
+        todo!()
+    }
+
+    pub fn clear(&mut self) {
+        self.indices.clear();
+        self.vertices.clear();
+        self.normals.clear();
+        self.uvs.0.clear();
+        self.uvs.1.clear();
+        self.uvs.2.clear();
+        self.uvs.3.clear();
+    }
+}
