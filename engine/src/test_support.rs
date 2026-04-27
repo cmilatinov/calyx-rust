9adee7e5b49f5caedeafe9c7ff7a672519885dbb
Cristian Milatinov <milatinovcristian@gmail.com>
2026-04-27 12:35:12 -0400

    feat: add headless integration test framework

---
+++ engine/src/test_support.rs
@@ -0,0 +1,531 @@
+use crate::assets::AssetRegistry;
+use crate::class_registry::ComponentRegistry;
+use crate::component::{Component, ComponentID};
+use crate::context::{AssetContext, GameContext, ReadOnlyRegistryContext, RegistryContext};
+use crate::core::Ref;
+use crate::input::{ActionMap, Input, InputState};
+use crate::reflect::type_registry::TypeRegistry;
+use crate::render::RenderContext;
+use crate::resource::ResourceMap;
+use crate::scene::{GameObject, Scene};
+use egui::{Event, Key, Modifiers, PointerButton, Pos2, RawInput, Rect, Vec2};
+use egui_wgpu::wgpu;
+use nalgebra_glm::{distance, Mat4, Vec3};
+use std::fmt::Debug;
+use std::path::PathBuf;
+use std::sync::Arc;
+use std::time::Duration;
+
+const DEFAULT_TEST_SCREEN_SIZE: Vec2 = Vec2::new(1280.0, 720.0);
+const MAX_CONNECT_ITERS: usize = 100;
+
+pub const FIXED_TEST_STEP_SECONDS: f32 = 1.0 / 60.0;
+
+pub fn test_render_context() -> Arc<RenderContext> {
+    let instance = wgpu::Instance::new(&wgpu::InstanceDescriptor {
+        backends: wgpu::Backends::all(),
+        ..Default::default()
+    });
+    let adapter = pollster::block_on(instance.request_adapter(&wgpu::RequestAdapterOptions {
+        power_preference: wgpu::PowerPreference::LowPower,
+        compatible_surface: None,
+        force_fallback_adapter: false,
+    }))
+    .expect("no wgpu adapter found");
+    let (device, queue) = pollster::block_on(adapter.request_device(&Default::default(), None))
+        .expect("failed to create wgpu device");
+    Arc::new(RenderContext::headless(Arc::new(device), Arc::new(queue)))
+}
+
+fn build_type_registry() -> Ref<TypeRegistry> {
+    let mut type_registry = TypeRegistry::new();
+    for f in inventory::iter::<crate::ReflectRegistrationFn> {
+        (f.function)(&mut type_registry);
+    }
+    Ref::new(type_registry)
+}
+
+pub fn test_registries() -> ReadOnlyRegistryContext {
+    let render_context = test_render_context();
+    let type_registry = build_type_registry();
+    let component_registry = Ref::new(ComponentRegistry::new(&type_registry.read()));
+    let asset_registry = AssetRegistry::new_test(
+        PathBuf::from("."),
+        render_context,
+        type_registry.clone(),
+        component_registry.clone(),
+    );
+    ReadOnlyRegistryContext {
+        assets: asset_registry.readonly(),
+        types: type_registry.readonly(),
+        components: component_registry.readonly(),
+    }
+}
+
+pub fn test_registries_with_assets(asset_paths: Vec<PathBuf>) -> ReadOnlyRegistryContext {
+    let render_context = test_render_context();
+    let type_registry = build_type_registry();
+    let component_registry = Ref::new(ComponentRegistry::new(&type_registry.read()));
+    let asset_registry = AssetRegistry::new_test_with_assets(
+        asset_paths,
+        render_context,
+        type_registry.clone(),
+        component_registry.clone(),
+    );
+    ReadOnlyRegistryContext {
+        assets: asset_registry.readonly(),
+        types: type_registry.readonly(),
+        components: component_registry.readonly(),
+    }
+}
+
+pub fn test_asset_context_with_assets(asset_paths: Vec<PathBuf>) -> AssetContext {
+    let render_context = test_render_context();
+    let type_registry = build_type_registry();
+    let component_registry = Ref::new(ComponentRegistry::new(&type_registry.read()));
+    let asset_registry = AssetRegistry::new_test_with_assets(
+        asset_paths,
+        render_context.clone(),
+        type_registry.clone(),
+        component_registry.clone(),
+    );
+    AssetContext {
+        render_context,
+        registries: RegistryContext {
+            types: type_registry,
+            components: component_registry,
+            assets: asset_registry,
+        },
+    }
+}
+
+pub fn test_scene() -> Scene {
+    test_registries().scene()
+}
+
+pub fn test_game_context() -> GameContext {
+    let render_context = test_render_context();
+    let type_registry = build_type_registry();
+    let component_registry = Ref::new(ComponentRegistry::new(&type_registry.read()));
+    let asset_registry = AssetRegistry::new_test(
+        PathBuf::from("."),
+        render_context.clone(),
+        type_registry.clone(),
+        component_registry.clone(),
+    );
+    let assets = AssetContext {
+        render_context,
+        registries: RegistryContext {
+            types: type_registry,
+            components: component_registry,
+            assets: asset_registry,
+        },
+    };
+    GameContext::new(assets)
+}
+
+pub struct SceneBuilder {
+    scene: Scene,
+}
+
+impl SceneBuilder {
+    pub fn new(registries: ReadOnlyRegistryContext) -> Self {
+        Self {
+            scene: registries.scene(),
+        }
+    }
+
+    pub fn spawn(&mut self, name: impl Into<String>) -> GameObject {
+        self.spawn_with_parent(None, name)
+    }
+
+    pub fn spawn_child(&mut self, parent: GameObject, name: impl Into<String>) -> GameObject {
+        self.spawn_with_parent(Some(parent), name)
+    }
+
+    pub fn add_component<T: Component + Send + Sync + 'static>(
+        &mut self,
+        game_object: GameObject,
+        component: T,
+    ) -> &mut Self {
+        self.scene.add_component(game_object, component);
+        self
+    }
+
+    pub fn set_transform(&mut self, game_object: GameObject, matrix: impl Into<Mat4>) -> &mut Self {
+        self.scene.set_transform(game_object, &matrix.into());
+        self
+    }
+
+    pub fn scene(&self) -> &Scene {
+        &self.scene
+    }
+
+    pub fn scene_mut(&mut self) -> &mut Scene {
+        &mut self.scene
+    }
+
+    pub fn finish(self) -> Scene {
+        self.scene
+    }
+
+    fn spawn_with_parent(
+        &mut self,
+        parent: Option<GameObject>,
+        name: impl Into<String>,
+    ) -> GameObject {
+        let component_id = ComponentID {
+            name: name.into(),
+            ..Default::default()
+        };
+        self.scene.create(Some(component_id), parent)
+    }
+}
+
+pub struct HeadlessSceneRunner {
+    registries: ReadOnlyRegistryContext,
+    resources: ResourceMap,
+    scene: Scene,
+    input_context: egui::Context,
+    action_map: ActionMap,
+    input_active: bool,
+    pending_events: Vec<Event>,
+    modifiers: Modifiers,
+    screen_rect: Rect,
+    step_seconds: f32,
+}
+
+impl HeadlessSceneRunner {
+    pub fn new() -> Self {
+        Self::from_scene(test_scene())
+    }
+
+    pub fn from_scene(scene: Scene) -> Self {
+        Self {
+            registries: scene.registries().clone(),
+            resources: ResourceMap::new(),
+            scene,
+            input_context: egui::Context::default(),
+            action_map: ActionMap::default(),
+            input_active: true,
+            pending_events: Vec::new(),
+            modifiers: Modifiers::default(),
+            screen_rect: Rect::from_min_size(Pos2::ZERO, DEFAULT_TEST_SCREEN_SIZE),
+            step_seconds: FIXED_TEST_STEP_SECONDS,
+        }
+    }
+
+    pub fn load_scene(&mut self, scene: Scene) {
+        self.registries = scene.registries().clone();
+        self.scene = scene;
+    }
+
+    pub fn scene(&self) -> &Scene {
+        &self.scene
+    }
+
+    pub fn scene_mut(&mut self) -> &mut Scene {
+        &mut self.scene
+    }
+
+    pub fn resources(&self) -> &ResourceMap {
+        &self.resources
+    }
+
+    pub fn resources_mut(&mut self) -> &mut ResourceMap {
+        &mut self.resources
+    }
+
+    pub fn action_map_mut(&mut self) -> &mut ActionMap {
+        &mut self.action_map
+    }
+
+    pub fn set_input_active(&mut self, active: bool) {
+        self.input_active = active;
+    }
+
+    pub fn set_step_seconds(&mut self, seconds: f32) {
+        self.step_seconds = seconds;
+    }
+
+    pub fn set_modifiers(&mut self, modifiers: Modifiers) {
+        self.modifiers = modifiers;
+    }
+
+    pub fn enqueue_event(&mut self, event: Event) {
+        self.pending_events.push(event);
+    }
+
+    pub fn press_key(&mut self, key: Key) {
+        self.enqueue_event(Event::Key {
+            key,
+            physical_key: Some(key),
+            pressed: true,
+            repeat: false,
+            modifiers: self.modifiers,
+        });
+    }
+
+    pub fn release_key(&mut self, key: Key) {
+        self.enqueue_event(Event::Key {
+            key,
+            physical_key: Some(key),
+            pressed: false,
+            repeat: false,
+            modifiers: self.modifiers,
+        });
+    }
+
+    pub fn move_pointer(&mut self, position: Pos2) {
+        self.enqueue_event(Event::PointerMoved(position));
+    }
+
+    pub fn press_pointer_button(&mut self, position: Pos2, button: PointerButton) {
+        self.enqueue_event(Event::PointerButton {
+            pos: position,
+            button,
+            pressed: true,
+            modifiers: self.modifiers,
+        });
+    }
+
+    pub fn release_pointer_button(&mut self, position: Pos2, button: PointerButton) {
+        self.enqueue_event(Event::PointerButton {
+            pos: position,
+            button,
+            pressed: false,
+            modifiers: self.modifiers,
+        });
+    }
+
+    pub fn prepare(&mut self) {
+        self.scene.prepare();
+    }
+
+    pub fn step(&mut self) {
+        self.prepare();
+        self.resources.time_mut().advance_by(self.step_seconds);
+
+        let raw_input = RawInput {
+            screen_rect: Some(self.screen_rect),
+            time: Some(self.resources.time().static_time() as f64),
+            predicted_dt: self.step_seconds,
+            modifiers: self.modifiers,
+            events: std::mem::take(&mut self.pending_events),
+            ..Default::default()
+        };
+        self.input_context.begin_pass(raw_input);
+
+        let input = Input::from_ctx(
+            &self.input_context,
+            None,
+            InputState {
+                is_active: self.input_active,
+                last_cursor_pos: None,
+                action_map: self.action_map.clone(),
+            },
+        );
+        self.scene
+            .update(&self.registries, &mut self.resources, &input);
+        let _ = self.input_context.end_pass();
+    }
+
+    pub fn step_many(&mut self, step_count: usize) {
+        for _ in 0..step_count {
+            self.step();
+        }
+    }
+}
+
+pub struct NetworkTestHarness {
+    pub contexts: Vec<GameContext>,
+}
+
+impl NetworkTestHarness {
+    pub fn new(client_count: usize) -> Self {
+        let mut host = test_game_context();
+        host.resources
+            .network_mut()
+            .host("127.0.0.1:0".parse().unwrap())
+            .expect("failed to host");
+
+        let server_addr = host
+            .resources
+            .network()
+            .server
+            .as_ref()
+            .unwrap()
+            .bound_addr();
+
+        let mut contexts = vec![host];
+        for _ in 0..client_count {
+            let mut ctx = test_game_context();
+            ctx.resources
+                .network_mut()
+                .client
+                .connect(server_addr)
+                .expect("failed to connect");
+            contexts.push(ctx);
+        }
+
+        Self { contexts }
+    }
+
+    pub fn host(&self) -> &GameContext {
+        &self.contexts[0]
+    }
+
+    pub fn host_mut(&mut self) -> &mut GameContext {
+        &mut self.contexts[0]
+    }
+
+    pub fn client(&self, index: usize) -> &GameContext {
+        &self.contexts[index + 1]
+    }
+
+    pub fn client_mut(&mut self, index: usize) -> &mut GameContext {
+        &mut self.contexts[index + 1]
+    }
+
+    pub fn pump(&mut self) {
+        for ctx in &mut self.contexts {
+            ctx.update();
+        }
+    }
+
+    pub fn wait_connected(&mut self) -> bool {
+        for _ in 0..MAX_CONNECT_ITERS {
+            self.pump();
+            let all_connected = self.contexts[1..]
+                .iter()
+                .all(|ctx| ctx.resources.network().client.is_connected());
+            if all_connected {
+                return true;
+            }
+            std::thread::sleep(Duration::from_millis(1));
+        }
+        false
+    }
+
+    pub fn client_count(&self) -> usize {
+        self.contexts.len() - 1
+    }
+}
+
+pub fn assert_entity_exists(scene: &Scene, game_object: GameObject) {
+    assert!(
+        scene.entry(game_object).is_some(),
+        "expected game object `{}` to exist",
+        scene.name(game_object)
+    );
+}
+
+pub fn assert_position_near(
+    scene: &Scene,
+    game_object: GameObject,
+    expected: Vec3,
+    tolerance: f32,
+) {
+    let actual = scene.world_transform(game_object).position;
+    let delta = distance(&actual, &expected);
+    assert!(
+        delta <= tolerance,
+        "expected `{}` at {:?} +/- {}, got {:?}",
+        scene.name(game_object),
+        expected,
+        tolerance,
+        actual
+    );
+}
+
+pub fn assert_component_value<T, V, F>(
+    scene: &Scene,
+    game_object: GameObject,
+    reader: F,
+    expected: V,
+) where
+    T: Component,
+    V: Debug + PartialEq,
+    F: FnOnce(&T) -> V,
+{
+    let actual = scene
+        .read_component::<T, _, _>(game_object, reader)
+        .unwrap_or_else(|| {
+            panic!(
+                "expected component `{}` on `{}`",
+                std::any::type_name::<T>(),
+                scene.name(game_object)
+            )
+        });
+    assert_eq!(
+        actual,
+        expected,
+        "unexpected `{}` component value on `{}`",
+        std::any::type_name::<T>(),
+        scene.name(game_object)
+    );
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{test_registries, HeadlessSceneRunner, SceneBuilder};
+    use crate as engine;
+    use crate::component::{
+        Component, ComponentEventContext, ComponentUpdate, ReflectComponent, ReflectComponentUpdate,
+    };
+    use crate::input::Input;
+    use crate::reflect::{Reflect, ReflectDefault};
+    use crate::resource::{Resource, ResourceMap};
+    use crate::utils::{ReflectTypeUuidDynamic, TypeUuid};
+    use egui::Key;
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Default, Resource)]
+    struct InputProbe {
+        shoot_presses: usize,
+        forward_axis: f32,
+    }
+
+    #[derive(Default, TypeUuid, Serialize, Deserialize, Component, Reflect)]
+    #[uuid = "8336b78b-5e57-474a-95ce-904bd42b5135"]
+    #[reflect(Default, TypeUuidDynamic, Component, ComponentUpdate)]
+    #[repr(C)]
+    struct TestInputComponent;
+
+    impl Component for TestInputComponent {}
+
+    impl ComponentUpdate for TestInputComponent {
+        fn update(&self, _ctx: ComponentEventContext, resources: &mut ResourceMap, input: &Input) {
+            let probe = resources.resource_mut::<InputProbe>().unwrap();
+            if input.action("shoot").just_pressed() {
+                probe.shoot_presses += 1;
+            }
+            probe.forward_axis = input.axis("move_forward");
+        }
+    }
+
+    #[test]
+    fn headless_runner_applies_queued_input_events() {
+        let mut builder = SceneBuilder::new(test_registries());
+        let game_object = builder.spawn("input-probe");
+        builder.add_component(game_object, TestInputComponent);
+
+        let mut runner = HeadlessSceneRunner::from_scene(builder.finish());
+        runner.resources_mut().insert(InputProbe::default());
+
+        runner.press_key(Key::W);
+        runner.press_key(Key::Space);
+        runner.step();
+
+        let probe = runner.resources().resource::<InputProbe>().unwrap();
+        assert_eq!(probe.shoot_presses, 1);
+        assert_eq!(probe.forward_axis, 1.0);
+
+        runner.release_key(Key::Space);
+        runner.release_key(Key::W);
+        runner.step();
+
+        let probe = runner.resources().resource::<InputProbe>().unwrap();
+        assert_eq!(probe.forward_axis, 0.0);
+    }
+}
